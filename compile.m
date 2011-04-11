2753751dc8b343f65791f9beedfffca600439d1a
Zdenek Kalal <zdenek.kalal@gmail.com>
2011-04-11 10:01:01 +0200

    Mac OS X (by noomerikal)

--- compile.m
+++ compile.m
@@ -1,45 +1,72 @@
 % Copyright 2011 Zdenek Kalal
 %
 % This file is part of TLD.
-% 
+%
 % TLD is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % TLD is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with TLD.  If not, see <http://www.gnu.org/licenses/>.
 
 % Compiles mex files.
 clc; clear all; cd mex;
 
-% edit based on your instalation folder
-include = ' -Ic:\OpenCV2.2\include\opencv\ -Ic:\OpenCV2.2\include\';
-libpath = 'C:\OpenCV2.2\CMake\lib\Release\';
-
-% =========================================================================
-
-lib = [];
-files = dir([libpath '*.lib']);
-for i = 1:length(files)
-    lib = [lib ' ' libpath files(i).name];
+if ispc
+    disp('PC');
+    include = ' -Ic:\OpenCV2.2\include\opencv\ -Ic:\OpenCV2.2\include\';
+    libpath = 'c:\OpenCV2.2\lib\';
+    files = dir([libpath '*.lib']);
+    
+    lib = [];
+    for i = 1:length(files),
+        lib = [lib ' ' libpath files(i).name];
+    end
+    
+    eval(['mex lk.cpp -O' include lib]);
+    mex -O -c tld.cpp
+    mex -O fern.cpp tld.obj
+    mex -O linkagemex.cpp
+    mex -O bb_overlap.cpp
+    mex -O warp.cpp
+    mex -O distance.cpp
 end
-eval(['mex lk.cpp -O' include lib]);
-mex -O -c tld.cpp
-mex -O fern.cpp tld.obj
-mex -O linkagemex.cpp  
-mex -O bb_overlap.cpp  
-mex -O warp.cpp        
-mex -O distance.cpp    
+
+if ismac
+    disp('Mac');
+    
+    include = ' -I/opt/local/include/opencv/ -I/opt/local/include/'; % /opt/local -> /usr/local 
+    libpath = '/opt/local/lib/'; % /opt/local -> /usr/local 
+    
+    files = dir([libpath 'libopencv*.dylib']);
+    
+    lib = [];
+    for i = 1:length(files),
+        lib = [lib ' ' libpath files(i).name];
+    end
+    
+    eval(['mex lk.cpp -O' include lib]);
+    mex -O -c tld.cpp
+    mex -O fern.cpp tld.o
+    mex -O linkagemex.cpp
+    mex -O bb_overlap.cpp
+    mex -O warp.cpp
+    mex -O distance.cpp
+    
+end
+
+if isunix
+    disp('Unix');
+    % to come
+end
+
 
 cd ..
-% clc;
 disp('Compilation finished.');
-
-
 
