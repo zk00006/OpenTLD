e66daf409bfa37bb49613cee524b16ce919524a8
Zdenek Kalal <zdenek.kalal@gmail.com>
2011-04-11 10:08:34 +0200

    Compilation for Mac OS X added (by noomerikal).

--- compile.m
+++ compile.m
@@ -15,7 +15,7 @@
 % You should have received a copy of the GNU General Public License
 % along with TLD.  If not, see <http://www.gnu.org/licenses/>.
 
-% Compiles mex files.
+% Compiles mex files
 clc; clear all; cd mex;
 
 if ispc
