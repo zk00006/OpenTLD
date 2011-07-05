64bc41e18eb2d9c3b73dd7e01581d1e649a88a4a
alantrrs <alan.trrs.ngls@gmail.com>
2011-07-05 13:26:24 -0500

    added using namespace std

--- include/LKTracker.h
+++ include/LKTracker.h
@@ -4,22 +4,22 @@
 
 class LKTracker{
 private:
-  vector<cv::Point2f> pointsFB;
+  std::vector<cv::Point2f> pointsFB;
   cv::Size window_size;
   int level;
-  vector<uchar> status;
-  vector<uchar> FB_status;
-  vector<float> sim_error;
-  vector<float> FB_error;
+  std::vector<uchar> status;
+  std::vector<uchar> FB_status;
+  std::vector<float> sim_error;
+  std::vector<float> FB_error;
   float simmed;
   float fbmed;
   cv::TermCriteria term_criteria;
   float lambda;
-  bool filterPts(vector<cv::Point2f>& points1,vector<cv::Point2f>& points2);
+  bool filterPts(std::vector<cv::Point2f>& points1,std::vector<cv::Point2f>& points2);
 public:
   LKTracker();
   bool trackf2f(const cv::Mat& img1, const cv::Mat& img2,
-                vector<cv::Point2f> &points1, vector<cv::Point2f> &points2);
+                std::vector<cv::Point2f> &points1, std::vector<cv::Point2f> &points2);
   float getFB(){return fbmed;}
 };
 
--- include/tld_utils.h
+++ include/tld_utils.h
@@ -3,11 +3,11 @@
 
 void drawBox(cv::Mat& image, CvRect box, cv::Scalar color = cvScalarAll(255), int thick=1); 
 
-void drawPoints(cv::Mat& image, vector<cv::Point2f> points,cv::Scalar color=cv::Scalar::all(255));
+void drawPoints(cv::Mat& image, std::vector<cv::Point2f> points,cv::Scalar color=cv::Scalar::all(255));
 
 cv::Mat createMask(const cv::Mat& image, CvRect box);
 
-float median(vector<float> v);
+float median(std::vector<float> v);
 
-vector<int> index_shuffle(int begin,int end);
+std::vector<int> index_shuffle(int begin,int end);
 
