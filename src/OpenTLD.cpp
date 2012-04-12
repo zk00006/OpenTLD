23cef6721ddf535b13845ff452a97bffafe811bf
Georg Nebehay <gnebehay@gmail.com>
2012-04-12 13:50:36 +0200

    Refactored Project and added OpenTLDConfig.cmake

---
+++ src/OpenTLD.cpp
@@ -0,0 +1,36 @@
+#include "Main.h"
+#include "config.h"
+#include "imAcq.h"
+#include "gui.h"
+
+
+int main(int argc, char ** argv) {
+
+	Main* main = new Main();
+	Config config;
+	ImAcq * imAcq = imAcqAlloc();
+	Gui * gui = new Gui();
+
+	main->gui = gui;
+	main->imAcq = imAcq;
+
+	if(config.init(argc, argv) == PROGRAM_EXIT) {
+		return EXIT_FAILURE;
+	}
+
+	config.configure(main);
+
+	srand(main->seed);
+
+	imAcqInit(imAcq);
+
+	if(main->showOutput) {
+		gui->init();
+	}
+
+	main->doWork();
+
+	delete main;
+
+	return EXIT_SUCCESS;
+}
