d30b88e077bb6286e42ff50a6fb9499231061575
DMRoberts <danmroberts@gmail.com>
2013-07-31 21:07:57 -0400

    Update img_blur.m
    MATLAB v >= 2011b are case sensitive with respect to function names.

--- img/img_blur.m
+++ img/img_blur.m
@@ -26,7 +26,7 @@ csize = 6*sigma;
 
 shift = (csize - 1)/2;
 
-h = FSPECIAL('gaussian',csize,sigma);
+h = fspecial('gaussian',csize,sigma);
 
 M = conv2(M,h);
 M = M(1+shift:end-shift,1+shift:end-shift);
